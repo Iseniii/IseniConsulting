fce45d55a10064f2c1f2f325916329325b2eaac5
Iseniii <99469886+Iseniii@users.noreply.github.com>
2022-03-16 00:45:31 +0100

    Add files via upload

---
+++ showhideTab.js
@@ -0,0 +1,43 @@
+function showhideTab(executionContext) {
+    formContext = executionContext.getFormContext();
+
+    let fieldvalue = "";
+    let tabs ={"tabname": ""};
+
+
+    switch(fieldvalue){
+
+        case "1":
+            tabs ={"tabname": "t1", "visible":false};
+            break;
+        case "2":
+            tabs ={"tabname": "t1", "visible":false};
+
+            tabs.forEach(element => {tabVisibility(element,)
+                
+            });
+            break;
+
+
+
+
+
+    }
+
+
+    
+    
+
+
+
+    function tabVisibility(tName,boolVisible){
+        let tabObj = formContext.ui.tabs.get(tName);
+        tabObj.setVisible(boolVisible);
+
+    }
+
+
+}
+
+
+   
